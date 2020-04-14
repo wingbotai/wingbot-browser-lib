ea9f4c350f0fa33a82314e064c50e464a8ffee62
David Menger <david.menger@me.com>
2020-04-14 21:10:50 +0200

    revert

--- src/botchat/wingbotBotChat.js
+++ src/botchat/wingbotBotChat.js
@@ -1,4 +1,24 @@
 
+const DIGITS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzčř'.split('');
+
+function encode (z) {
+    let r = '';
+    let n = (z || (Math.random() * 4294967294)) - 2147483647;
+    do {
+        r = DIGITS[n & 0x3f] + r; // eslint-disable-line no-bitwise
+        n >>>= 6; // eslint-disable-line no-bitwise
+    } while (n !== 0);
+    return r;
+}
+
+function randomId () {
+    let r = '';
+    for (let k = 0; k < 12; k++) {
+        r += encode();
+    }
+    return `${encode(Math.floor(Date.now() / 1000))}${r}`.substr(0, 64);
+}
+
 function setCookie (name, val, path = '/', secure = false) {
     const scr = secure ? ';secure' : '';
     document.cookie = `${name}=${val};path=${path};expires=Fri, 31 Dec 9999 23:59:59 GMT;${scr}sameSite=lax;`;
@@ -9,11 +29,18 @@ function createDlAndRenderComponent (
     componentCfg,
     targetEl,
     onAttach,
+    userId = null,
     conversationId = null
 ) {
     let init = false;
     let started = false;
+    let uid = userId;
     let cid = conversationId;
+
+    if (!uid) {
+        uid = randomId();
+        setCookie('wingbotUserId', uid, cfg.path, cfg.secure);
+    }
 
     const directLineConfig = { secret: cfg.secret };
 
@@ -31,6 +58,7 @@ function createDlAndRenderComponent (
         const activity = {
             type: 'event',
             name: 'postBack',
+            from: { id: uid, name: cfg.userName || 'You' },
             value: { action, data }
         };
         if (started) {
@@ -58,7 +86,7 @@ function createDlAndRenderComponent (
         dl.connectionStatus$.subscribe((cs) => {
             if (cs === 4) {
                 // it failed, we'll re-start the component
-                createDlAndRenderComponent(cfg, componentCfg, targetEl, onAttach);
+                createDlAndRenderComponent(cfg, componentCfg, targetEl, onAttach, uid);
             } else if (cs === 2) {
                 processQueue();
                 if (!init) {
@@ -72,7 +100,7 @@ function createDlAndRenderComponent (
         dl.connectionStatus$.subscribe((cs) => {
             if (cs === 4) {
                 // it failed, we'll re-start the component
-                createDlAndRenderComponent(cfg, componentCfg, targetEl, onAttach);
+                createDlAndRenderComponent(cfg, componentCfg, targetEl, onAttach, uid);
             } else if (cs === 2) {
                 processQueue();
                 onAttach(dl);
@@ -99,7 +127,7 @@ function createDlAndRenderComponent (
         targetEl.appendChild(into);
 
         const cmpc = componentCfg || {};
-        cmpc.user = { name: cfg.userName || 'You' };
+        cmpc.user = { id: uid, name: cfg.userName || 'You' };
         cmpc.botConnection = dl;
 
         // @ts-ignore
@@ -114,6 +142,7 @@ function createDlAndRenderComponent (
 /**
  * @param {Object} cfg
  * @param {string} cfg.secret - botservice secret
+ * @param {string} [cfg.userID] - pass custom user id
  * @param {string} [cfg.conversationID] - pass custom conversation ID
  * @param {string} [cfg.userName] - pass custom user name
  * @param {string} [cfg.initAction] - path of the wingbot init action
@@ -128,7 +157,17 @@ function createDlAndRenderComponent (
  * @returns {Object} - DirectLine object with new postBack(action[,data={}]) method
  */
 export default function wingbotBotChat (cfg, componentCfg, targetEl, onAttach = () => {}) {
+    const uidMatch = (document.cookie || '').match(/wingbotUserId=([^&;\s]+)/i);
     const cidMatch = (document.cookie || '').match(/wingbotCid=([^&;\s]+)/i);
+
+    let uid = null;
+
+    if (cfg.userID) {
+        uid = cfg.userID;
+        setCookie('wingbotUserId', uid, cfg.path, cfg.secure);
+    } else if (uidMatch) {
+        [, uid] = uidMatch;
+    }
 
 
     let cid = null;
@@ -140,5 +179,5 @@ export default function wingbotBotChat (cfg, componentCfg, targetEl, onAttach = 
         [, cid] = cidMatch;
     }
 
-    return createDlAndRenderComponent(cfg, componentCfg, targetEl, onAttach, cid);
+    return createDlAndRenderComponent(cfg, componentCfg, targetEl, onAttach, uid, cid);
 }
